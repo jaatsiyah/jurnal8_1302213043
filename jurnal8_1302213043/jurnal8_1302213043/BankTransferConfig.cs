fe14597c6eaa4c1f57c509d39f526cc03bc096fe
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-04-11 17:13:34 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ jurnal8_1302213043/jurnal8_1302213043/BankTransferConfig.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using System.Text.Json;
+
+public class BankTransferConfig
+{
+	public string lang { get; set; }
+	public transfer transfer { get; set; }
+	public List<string> method { get; set; }
+
+	public confirmation confirmation { get; set; } 
+
+    public BankTransferConfig() { }
+
+    public BankTransferConfig(string lang, transfer transfer, List<string> method, confirmation confirmation)
+    {
+        this.lang = lang;
+        this.transfer = transfer;
+        this.method = method;
+        this.confirmation = confirmation;
+    }
+}
+
+public class transfer
+{
+	public int threshold { get; set;}
+	public int low_fee { get; set; }
+	public int high_fee { get; set;}
+
+	public transfer() { }
+
+	public transfer(int threshold, int low_fee, int high_fee)
+    {
+        this.threshold = threshold;
+        this.low_fee = low_fee;
+        this.high_fee = high_fee;
+    }	
+}
+
+public class confirmation
+{
+	public string en { get; set; }
+
+	public string id { get; set; }
+
+	public confirmation() { }
+
+	public confirmation(string en, string id)
+    {
+        this.en = en;
+        this.id = id;
+    }
+}
+
+class UIConfig
+{
+    public BankTransferConfig config;
+    public const string fileLocation = "D:\\TELKOM\\SEMESTER 4\\KONSTRUKSI PERANGKAT LUNAK\\jurnal8_1302213043\\jurnal8_1302213043\\jurnal8_1302213043\\";
+    public const string filePath = fileLocation + "bank_transfer_config.json";
+
+    public UIConfig()
+    {
+        try
+        {
+            ReadConfigFile();
+        }
+        catch (Exception)
+        {
+            setDefault();
+            writeNewConfigFile();
+        }
+    }
+
+    private BankTransferConfig ReadConfigFile()
+    {
+        string configJSONData = File.ReadAllText(filePath);
+        config = JsonSerializer.Deserialize<BankTransferConfig>(configJSONData);
+        return config;
+    }
+
+    private void setDefault()
+    {
+
+        transfer transfer = new transfer(25000000,6500,15000);
+        List<string> methods = new List<string> { "RTO (real-time)","SKN","RTGS","BI FAST"};
+        confirmation confirmation = new confirmation("yes","no");
+
+        config = new BankTransferConfig("en", transfer, methods, confirmation);
+    }
+
+    private void writeNewConfigFile()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        };
+
+        string jsonString = JsonSerializer.Serialize(config, options);
+        File.WriteAllText(filePath, jsonString);
+    }
+}
+
--- jurnal8_1302213043/jurnal8_1302213043/Program.cs
+++ jurnal8_1302213043/jurnal8_1302213043/Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        UIConfig uiConfig = new UIConfig();
+
+        if(uiConfig.config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer");
+
+        }else if(uiConfig.config.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer");
+        }
+
+        int amount = int.Parse(Console.ReadLine());
+
+        int fee;
+        if(amount <= uiConfig.config.transfer.threshold)
+        {
+            fee = uiConfig.config.transfer.low_fee;
+        }else
+        {
+            fee = uiConfig.config.transfer.high_fee;
+        }
+
+        if (uiConfig.config.lang == "en")
+        {
+            Console.WriteLine("Transfer fee = " + fee + "Total amount = " + (amount + fee));
+                
+
+        }
+        else if (uiConfig.config.lang == "id")
+        {
+            Console.WriteLine("Biaya transfer = " + fee + "Total biaya = " + (amount + fee));
+        }
+
+        if (uiConfig.config.lang == "en")
+        {
+            Console.WriteLine("Select transfer method : ");
+
+        }
+        else if (uiConfig.config.lang == "id")
+        {
+            Console.WriteLine("Pilih metode transfer : ");
+        }
+
+        for(int i = 0; i < uiConfig.config.method.Count; i++)
+        {
+            Console.WriteLine((i + 1) + "." + uiConfig.config.method[i]);
+        }
+
+        if (uiConfig.config.lang == "en")
+        {
+            Console.WriteLine("Please type " + uiConfig.config.confirmation.en + " to confirm the transaction :");
+
+        }
+        else if (uiConfig.config.lang == "id")
+        {
+            Console.WriteLine("Ketik " + uiConfig.config.confirmation.id + " untuk mengkonfirmasi transaksi :");
+        }
+
+        string confirm = Console.ReadLine();
+
+        if (uiConfig.config.lang == "en")
+        {
+            if (confirm == uiConfig.config.confirmation.en)
+            {
+                Console.WriteLine("The transfer is completed");
+            } else if (confirm != uiConfig.config.confirmation.en)
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+
+        }
+        else if (uiConfig.config.lang == "id")
+        {
+            if(confirm == uiConfig.config.confirmation.id)
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            } else if (confirm != uiConfig.config.confirmation.id)
+            {
+                Console.WriteLine("Transfer dibatalkan");
+            }
+        }
+
+        
+    }
+}
